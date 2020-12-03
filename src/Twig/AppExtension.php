37a3d13702885e180f2c3a5c2ff86e06d3c8f8b3
Jérôme Vieilledent <jerome.vieilledent@blackfire.io>
2020-12-03 12:38:41 +0100

    Add cache on top of user activity text

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,6 +4,8 @@ namespace App\Twig;
 
 use App\Entity\User;
 use App\Service\CommentHelper;
+use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -11,10 +13,12 @@ use Twig\TwigFunction;
 class AppExtension extends AbstractExtension
 {
     private $commentHelper;
+    private $cache;
 
-    public function __construct(CommentHelper $commentHelper)
+    public function __construct(CommentHelper $commentHelper, CacheInterface $cache)
     {
         $this->commentHelper = $commentHelper;
+        $this->cache = $cache;
     }
 
     public function getFilters(): array
@@ -25,6 +29,21 @@ class AppExtension extends AbstractExtension
     }
 
     public function getUserActivityText(User $user): string
+    {
+        $key = sprintf('user_activity_text_%s', $user->getId());
+
+        return $this->cache->get($key, function(CacheItemInterface $item) use ($user) {
+            $item->expiresAfter(3600);
+
+            return $this->calculateUserActivityText($user);
+        });
+    }
+
+    /**
+     * @param User $user
+     * @return string
+     */
+    private function calculateUserActivityText(User $user): string
     {
         $commentCount = $this->commentHelper->countRecentCommentsForUser($user);
 
