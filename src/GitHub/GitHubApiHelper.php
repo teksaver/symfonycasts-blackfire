9132e057426ba3e8d045ccd72a13ae7b6fc12c0f
Jérôme Vieilledent <jerome.vieilledent@blackfire.io>
2020-12-03 13:00:33 +0100

    Optimize Github API calls

--- src/GitHub/GitHubApiHelper.php
+++ src/GitHub/GitHubApiHelper.php
@@ -8,6 +8,8 @@ class GitHubApiHelper
 {
     private $httpClient;
 
+    private $githubOrganizations = [];
+
     public function __construct(HttpClientInterface $httpClient)
     {
         $this->httpClient = $httpClient;
@@ -15,11 +17,16 @@ class GitHubApiHelper
 
     public function getOrganizationInfo(string $organization): GitHubOrganization
     {
-        $response = $this->httpClient->request('GET', 'https://api.github.com/orgs/'.$organization);
+        // optimization in case getOrganizationRepositories is called first
+        if (isset($this->githubOrganizations[$organization])) {
+            return $this->githubOrganizations[$organization];
+        }
+
+        $response = $this->httpClient->request('GET', 'https://api.github.com/orgs/' . $organization);
 
         $data = $response->toArray();
 
-        return new GitHubOrganization(
+        return $this->githubOrganizations[$data['name']] = new GitHubOrganization(
             $data['name'],
             $data['public_repos']
         );
@@ -29,20 +36,32 @@ class GitHubApiHelper
      * @return GitHubRepository[]
      */
     public function getOrganizationRepositories(string $organization): array
-   {
-       $response = $this->httpClient->request('GET', sprintf('https://api.github.com/orgs/%s/repos', $organization));
+    {
+        $response = $this->httpClient->request('GET', sprintf('https://api.github.com/orgs/%s/repos', $organization));
 
-       $data = $response->toArray();
+        $data = $response->toArray();
 
-       $repositories = [];
-       foreach ($data as $repoData) {
-           $repositories[] = new GitHubRepository(
-               $repoData['name'],
-               $repoData['html_url'],
-               \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s\Z', $repoData['updated_at'])
-           );
-       }
+        $repositories = [];
+        $publicRepoCount = 0;
+        foreach ($data as $repoData) {
+            $repositories[] = new GitHubRepository(
+                $repoData['name'],
+                $repoData['html_url'],
+                \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s\Z', $repoData['updated_at'])
+            );
 
-       return $repositories;
-   }
+            if ($repoData['private'] === false) {
+                ++$publicRepoCount;
+            }
+        }
+
+        if (!isset($this->githubOrganizations[$organization])) {
+            $this->githubOrganizations[$organization] = new GitHubOrganization(
+                $data[0]['owner']['login'],
+                $publicRepoCount
+            );
+        }
+
+        return $repositories;
+    }
 }
