1bd833e62239b2a8f5dcf579dbcb5859937be38a
Sylvain Tenier <teksaver@yahoo.fr>
2022-04-06 12:01:45 +0200

    fix code

--- src/Service/CommentHelper.php
+++ src/Service/CommentHelper.php
@@ -3,20 +3,18 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Repository\CommentRepository;
 
 class CommentHelper
 {
+    // nouveau en PHP 8.1 : création d'un attribut par la variable déclarée
+    public function __construct(private CommentRepository $repository)
+    {   
+    }
+
     public function countRecentCommentsForUser(User $user): int
     {
-        $comments = $user->getComments();
-        $commentCount = 0;
-        $recentDate = new \DateTimeImmutable('-3 months');
-        foreach ($comments as $comment) {
-            if ($comment->getCreatedAt() > $recentDate) {
-                $commentCount++;
-            }
-        }
 
-        return $commentCount;
+        return $this->repository->countForUser($user, new \DateTimeImmutable('-3 months'));
     }
 }
